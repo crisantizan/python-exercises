a57929d9796b246bd52f5f5868f7ee01d5f09230
Cristian Santiz <crisantizan@gmail.com>
2020-07-30 23:36:00 -0500

    feat: generate random word

--- hangman.py
+++ hangman.py
@@ -1,3 +1,5 @@
+from random import randint
+
 CLIPS = [
     '''
             +---+
@@ -81,9 +83,18 @@ CLIPS = [
     ''',
 ]
 
+WORDS = ['house', 'computer', 'mouse', 'tree', 'country',
+         'microsoft', 'freezer', 'colombia', 'constitution', ]
+
+
+def random_word():
+    index = randint(0, len(WORDS)-1)
+    return WORDS[index]
+
 
 def main():
     print('***** Welcome to Hangman Game *****')
+    print(random_word())
 
 
 if __name__ == '__main__':
